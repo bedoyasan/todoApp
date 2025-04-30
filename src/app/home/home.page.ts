73a7aff70bf5bf0cdf0ee5e3b5536cd12a88e7b1
Santiago <snbedoyao@gmail.com>
2025-04-30 08:27:43 -0500

    Filtrar por categoría

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { NgClass, NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
 import {
@@ -13,8 +13,10 @@ import {
   IonItem,
   IonLabel,
   IonCardContent,
+  IonSelect,
+  IonSelectOption,
 } from '@ionic/angular/standalone';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -31,34 +33,80 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
     IonItem,
     IonLabel,
     IonCardContent,
+    IonSelect,
+    IonSelectOption,
     NgFor,
     ReactiveFormsModule,
-    NgClass,],
+    NgClass,
+    NgIf,
+    FormsModule,
+  ],
 })
-
 export class HomePage {
   formToDo: FormGroup;
   todoList: ToDo[] = [];
+  typeList: string[] = ["Trabajo", "Personal", "Otro..."]
+
+  filterValue: string = "";
   constructor(private formBuilder: FormBuilder) {
     this.formToDo = this.formBuilder.group({
       todofield: [''],
+      typefield: [''],
+      typecustomfield: [''],
     })
-
+    
     this.loadToDos();
+  }
+
+  setFilter(event: any) {
+    this.filterValue = event.detail.value;
+  }
+
+  resetFilter() {
+    this.filterValue = "";
+  }
+
+  getFilterValues() {
+    let filterData: string[] = [];
+    this.todoList.forEach(e => {
+      if(!filterData.includes(e.type)) {
+        filterData.push(e.type);
+      }
+    });
+
+    return filterData;
   }
 
   addToDo() {
     let newToDo = this.formToDo.value.todofield;
-    if (newToDo) {
+    let typeToDo = this.formToDo.value.typefield;
+    if (typeToDo === 'Otro...') {
+      typeToDo = this.formToDo.value.typecustomfield;
+      if (typeToDo) {
+        this.typeList = [
+          ...this.typeList.slice(0, this.typeList.length - 1),
+          typeToDo,
+          this.typeList[this.typeList.length - 1]
+        ]
+      }
+    }
+    if (newToDo && typeToDo) {
       this.todoList.push({
         id: uuidv4(),
         name: newToDo,
         completed: false,
+        type: typeToDo,
       })
       this.saveToDos();
       this.formToDo.reset();
+      this.resetFilter();
     }
 
+  }
+
+  removeType(typeName: string) {
+    this.typeList = this.typeList.filter(e => e !== typeName);
+    this.saveToDos();
   }
 
   completeToDo(id: string) {
@@ -73,12 +121,17 @@ export class HomePage {
 
   saveToDos() {
     localStorage.setItem('toDos', JSON.stringify(this.todoList));
+    localStorage.setItem('typeToDos', JSON.stringify(this.typeList));
   }
 
   loadToDos() {
     const savedTodos = localStorage.getItem('toDos');
     if (savedTodos) {
       this.todoList = JSON.parse(savedTodos);
+    }
+    const savedTypes = localStorage.getItem('typeToDos');
+    if (savedTypes) {
+      this.typeList = JSON.parse(savedTypes);
     }
   }
 }
@@ -87,4 +140,5 @@ export interface ToDo {
   id: string;
   name: string;
   completed: boolean;
+  type: string;
 }
